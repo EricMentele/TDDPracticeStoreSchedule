054937b7b09b544f03d31669566bbfc2476d3dd0
Eric Mentele <ericmentele@gmail.com>
2024-04-11 17:30:20 -0500

    Create MacOS project with tests

---
+++ StoreSchedule/StoreSchedule/StoreSchedule.h
@@ -0,0 +1,18 @@
+//
+//  StoreSchedule.h
+//  StoreSchedule
+//
+//  Created by Eric Mentele on 4/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoreSchedule.
+FOUNDATION_EXPORT double StoreScheduleVersionNumber;
+
+//! Project version string for StoreSchedule.
+FOUNDATION_EXPORT const unsigned char StoreScheduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreSchedule/PublicHeader.h>
+
+
